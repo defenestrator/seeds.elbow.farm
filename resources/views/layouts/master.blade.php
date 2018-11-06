c7c6b3f7a3edba042628d78a55d8eb0418b19c01
Jeremy Anderson <jeremy@edos.io>
2018-11-06 15:13:13 -0700

    push it real good

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -22,7 +22,7 @@
     <nav class="navbar navbar-expand-md navbar-light navbar-elbow">
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">
-                <img src="/i/heisenhead.png">
+                <img src="/i/heisenhead-60.png">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -38,8 +38,8 @@
                 <ul class="navbar-nav ml-auto">
                     <!-- Authentication Links -->
                     @guest
-                    {{--<li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>--}}
-                    {{--<li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>--}}
+                    <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                    <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
                     <li><a class="nav-link" href="{{ route('welcome') }}">{{ __('Welcome') }}</a></li>
 
 
