0f39195f28f625530ce5ee2f93c3b0e45eaf5d2e
Jeremy Anderson <jeremy@edos.io>
2018-11-13 13:39:48 -0700

    more database tables and models and shit.

--- app/SeedType.php
+++ app/SeedType.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class SeedType extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'description'
+    ];
+
+    public function strains()
+    {
+        return $this->hasMany(Strain::class);
+    }
 }
