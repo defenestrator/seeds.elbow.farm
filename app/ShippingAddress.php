db92de51c3fbfe84eec042714f6f4ea970b9ff03
Jeremy Anderson <jeremy@edos.io>
2018-11-14 08:48:35 -0700

    add more stuff for shipping addies

--- app/ShippingAddress.php
+++ app/ShippingAddress.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class ShippingAddress extends Model
 {
     protected $fillable = [
-        'address_1', 'address_2', 'city', 'state', 'postcode', 'country', 'user_id'
+       'ship_to_name', 'address_1', 'address_2', 'city', 'state', 'postcode', 'country', 'user_id'
     ];
 
     public function users()
