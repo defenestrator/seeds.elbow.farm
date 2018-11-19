0709ae08b4e9e602687427a7af8238dd292837b6
Jeremy Anderson <jeremy@edos.io>
2018-11-19 12:15:38 -0700

    quick upodates, more coming.

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Heisenbeans'),
+    'name' => env('APP_NAME', 'Heisenbeans - Feminized Seeds'),
 
     /*
     |--------------------------------------------------------------------------
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Heisenbeans</title>
+        <title>{{config('app.name')}}</title>
         <link href="https://fonts.googleapis.com/css?family=Great+Vibes"
               rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans"
