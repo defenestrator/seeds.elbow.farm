42ef6e9ca03150bfb4557abfa481b5db199cfa4b
Jeremy Anderson <jeremy@edos.io>
2019-01-29 20:00:30 -0700

    maybe?

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -38,7 +38,7 @@
                 <ul class="navbar-nav ml-auto">
                     <!-- Authentication Links -->
                     @guest
-                    {{--<li><a class="nav-link" href="{{ route('welcome') }}">{{ __('Welcome') }}</a></li>--}}
+                    <li><a class="nav-link" href="https://www.instagram.com/heisenbeans/" alt="Follow @heisenbeans on IG!" title="Follow @heisenbeans on IG!"><img src="http://i.heisenbeans.com/images/app-icon2-small.png"></a></li>
                     <li><a class="nav-link" href="{{ route('testers') }}">Testers</a></li>
                     <li><a class="nav-link" href="{{ route('breeders') }}">Breeders</a></li>
                     <li><a class="nav-link" href="{{ route('strains') }}">Strains</a></li>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,7 @@
     <div class="row justify-content-center">
         <h1>Heisenbeans</h1>
     </div>
-    <div class="row justify-content-center">
-        <h2><a style="text-align:center;" href="https://www.instagram.com/heisenbeans/"><h2>Follow us on IG <i class="fa fa-instagram" aria-hidden="true"></i></a></h2>
-    </div>
+
     <div class="row justify-content-center">
 
         <hr>
@@ -15,7 +13,12 @@
         <a href="/testers"><img src="/i/heroic-colas.jpg"></a>
     </div>
     <hr>
-    <div class="row justify-content-center"></div>
+    <div class="row justify-content-center">
+        <a style="text-align:center;" href="https://www.instagram.com/heisenbeans/">Follow us on IG</a>
+    </div>
+    <div class="row justify-content-center">
+        <a style="text-align:center;" href="https://www.instagram.com/heisenbeans/"><img src="http://i.heisenbeans.com/images/app-icon2-small.png"></a>
+    </div>
             @foreach(array_chunk(array_slice($posts, 0, 3), 3) as $postRow)
                 <div class="row justify-content-center">
                     @foreach($postRow as $post)
