a29aa819f744bd736af0bb531490a08e0022770a
Jeremy Anderson <jeremy@edos.io>
2019-02-02 08:00:28 -0700

    fix migration

--- database/migrations/2019_02_02_133340_add_permissions_and_roles_records_to_permissions_and_roles_tables.php
+++ database/migrations/2019_02_02_133340_add_permissions_and_roles_records_to_permissions_and_roles_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddPermissionsRecordsToPermissionsTable extends Migration
+class AddPermissionsAndRolesRecordsToPermissionsAndRolesTables extends Migration
 {
     /**
      * Run the migrations.
