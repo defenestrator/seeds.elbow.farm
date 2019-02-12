f8a82c696fdb5431fd91a677e1f5551e2fa3ac5c
The Defenestrator <529446+defenestrator@users.noreply.github.com>
2019-02-12 14:10:44 -0700

    media table migration and perms and roles fixes

--- database/migrations/2019_02_02_133340_add_permissions_and_roles_records_to_permissions_and_roles_tables.php
+++ database/migrations/2019_02_02_133340_add_permissions_and_roles_records_to_permissions_and_roles_tables.php
@@ -13,52 +13,7 @@ class AddPermissionsAndRolesRecordsToPermissionsAndRolesTables extends Migration
      */
     public function up()
     {
-        $create_roles = [
-            "php artisan permission:create-role admin",
-            "php artisan permission:create-role customer",
-            "php artisan permission:create-role vendor",
-            "php artisan permission:create-role author",
-            "php artisan permission:create-role publisher",
-            "php artisan permission:create-role moderator",
-        ];
-
-        $create_permissions = [
-            "php artisan permission:create-permission create-breeder",
-            "php artisan permission:create-permission edit-breeder",
-            "php artisan permission:create-permission delete-breeder",
-
-            "php artisan permission:create-permission create-image",
-            "php artisan permission:create-permission edit-image",
-            "php artisan permission:create-permission delete-image",
-
-            "php artisan permission:create-permission create-seed-type",
-            "php artisan permission:create-permission edit-seed-type",
-            "php artisan permission:create-permission delete-seed-type",
-
-            "php artisan permission:create-permission create-shipping-address",
-            "php artisan permission:create-permission edit-shipping-address",
-            "php artisan permission:create-permission delete-shipping-address",
-
-            "php artisan permission:create-permission create-strain",
-            "php artisan permission:create-permission edit-strain",
-            "php artisan permission:create-permission delete-strain",
-
-            "php artisan permission:create-permission edit-tester",
-            "php artisan permission:create-permission create-tester",
-            "php artisan permission:create-permission delete-tester",
-
-            "php artisan permission:create-permission create-user",
-            "php artisan permission:create-permission edit-user",
-            "php artisan permission:create-permission delete-user",
-        ];
-
-        foreach($create_roles as $create_role) {
-            exec($create_role);
-        }
-
-        foreach($create_permissions as $create_permission) {
-            exec($create_permission);
-        }
+        DB::unprepared(File::get('database/sql/perms-and-roles.sql'));
     }
 
     /**
---
+++ database/sql/perms-and-roles.sql
@@ -0,0 +1,81 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.24-0ubuntu0.18.04.1)
+# Database: heisen
+# Generation Time: 2019-02-10 15:34:38 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table permissions
+# ------------------------------------------------------------
+
+LOCK TABLES `permissions` WRITE;
+/*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
+
+INSERT INTO `permissions` (`id`, `name`, `guard_name`, `created_at`, `updated_at`)
+VALUES
+	(1,'create-breeder','web','2019-02-10 15:33:29','2019-02-10 15:33:29'),
+	(2,'edit-breeder','web','2019-02-10 15:33:31','2019-02-10 15:33:31'),
+	(3,'delete-breeder','web','2019-02-10 15:33:32','2019-02-10 15:33:32'),
+	(4,'create-image','web','2019-02-10 15:33:34','2019-02-10 15:33:34'),
+	(5,'edit-image','web','2019-02-10 15:33:35','2019-02-10 15:33:35'),
+	(6,'delete-image','web','2019-02-10 15:33:37','2019-02-10 15:33:37'),
+	(7,'create-seed-type','web','2019-02-10 15:33:38','2019-02-10 15:33:38'),
+	(8,'edit-seed-type','web','2019-02-10 15:33:40','2019-02-10 15:33:40'),
+	(9,'delete-seed-type','web','2019-02-10 15:33:41','2019-02-10 15:33:41'),
+	(10,'create-shipping-address','web','2019-02-10 15:33:43','2019-02-10 15:33:43'),
+	(11,'edit-shipping-address','web','2019-02-10 15:33:44','2019-02-10 15:33:44'),
+	(12,'delete-shipping-address','web','2019-02-10 15:33:46','2019-02-10 15:33:46'),
+	(13,'create-strain','web','2019-02-10 15:33:47','2019-02-10 15:33:47'),
+	(14,'edit-strain','web','2019-02-10 15:33:49','2019-02-10 15:33:49'),
+	(15,'delete-strain','web','2019-02-10 15:33:50','2019-02-10 15:33:50'),
+	(16,'edit-tester','web','2019-02-10 15:33:51','2019-02-10 15:33:51'),
+	(17,'create-tester','web','2019-02-10 15:33:53','2019-02-10 15:33:53'),
+	(18,'delete-tester','web','2019-02-10 15:33:54','2019-02-10 15:33:54'),
+	(19,'create-user','web','2019-02-10 15:33:56','2019-02-10 15:33:56'),
+	(20,'edit-user','web','2019-02-10 15:33:57','2019-02-10 15:33:57'),
+	(21,'delete-user','web','2019-02-10 15:33:59','2019-02-10 15:33:59');
+
+/*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table roles
+# ------------------------------------------------------------
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+
+INSERT INTO `roles` (`id`, `name`, `guard_name`, `created_at`, `updated_at`)
+VALUES
+	(1,'admin','web','2019-02-10 15:33:20','2019-02-10 15:33:20'),
+	(2,'customer','web','2019-02-10 15:33:21','2019-02-10 15:33:21'),
+	(3,'vendor','web','2019-02-10 15:33:23','2019-02-10 15:33:23'),
+	(4,'author','web','2019-02-10 15:33:24','2019-02-10 15:33:24'),
+	(5,'publisher','web','2019-02-10 15:33:26','2019-02-10 15:33:26'),
+	(6,'moderator','web','2019-02-10 15:33:27','2019-02-10 15:33:27');
+
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
