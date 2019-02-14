1d86ca6194901b4f0825e6c9880c7235bee5d49d
The Defenestrator <529446+defenestrator@users.noreply.github.com>
2019-02-14 13:24:35 -0700

    Fix me!

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -56,11 +56,11 @@
 
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
 
-                                @can('admin', Strain::class)
+                                {{-- @can('admin', Strain::class)
                                     <a class="dropdown-item" href="{{ route('admin.strains.list') }}">Edit Strains</a>
                                     <a class="dropdown-item" href="{{ route('admin.strains.create') }}">New Strain</a>
                                     <hr>
-                                @endcan
+                                @endcan --}}
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
