e423a48e49ded98544b4e27bbac72da3ae3f653a
The Defenestrator <529446+defenestrator@users.noreply.github.com>
2019-03-03 12:20:25 -0700

    more updates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -52,13 +52,16 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
+                            <div class="col-lg-8 offset-lg-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
 
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
                                     {{ __('Forgot Your Password?') }}
+                                </a>
+                                <a class="btn btn-link" href="{{ route('register') }}">
+                                    {{ __('Register') }}
                                 </a>
                             </div>
                         </div>
