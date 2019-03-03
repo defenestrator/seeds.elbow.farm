5bdee972c34a02a5afcb7f1b7483bf339d2aadb1
The Defenestrator <529446+defenestrator@users.noreply.github.com>
2019-03-03 12:44:35 -0700

    damn responseive images

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
             @foreach($strainRow as $strain)
             <div class="col-md-4 info">
                 <h4><a href="/strains/{{$strain['id']}}">{{$strain['name']}}</a></h4>
-                <a href="/strains/{{$strain['id']}}"><div style="min-height:330px; max-height:440px; background-position:cover; background-repeat:no-repeat; background-image:url('{{ $strain["image"] }}'"></div></a>
+                <a href="/strains/{{$strain['id']}}"><div style="min-height:330px; max-height:800px; background-position: contain; background:url('{{ $strain["image"] }}') center center no-repeat;"></div></a>
 
                     <form action="POST" target="/cart">
                             @csrf
