462d7e4bb61e2e379b424f0096fb6e27baa8e173
The Defenestrator <529446+defenestrator@users.noreply.github.com>
2019-03-13 11:22:15 -0600

    redirect login

--- resources/views/strains/create.blade.php
+++ resources/views/strains/create.blade.php
@@ -6,9 +6,9 @@
 @stop
 
 @section('page_styles')
-    <style>
+<style>
 
-    </style>
+</style>
 @stop
 
 @section('top')
@@ -21,11 +21,11 @@
     <div class="row col-md-12 alert alert-danger">
         <ul>
             @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
+            <li>{{ $error }}</li>
             @endforeach
         </ul>
     </div>
-@endif
+    @endif
 
     <div class="row">
         <div class="col-md-12">
@@ -41,23 +41,27 @@
                 @csrf
                 <div class="form-group row col-md-12">
                     <label class="btn btn-warning btn-file">
-                        Choose Image <input type="file" style="display: none;" onchange="previewFile()" name="image" value="{{old('image')}}" >
+                        Choose Image <input type="file" style="display: none;" onchange="previewFile()" name="image"
+                            value="{{old('image')}}">
                     </label>
                 </div>
                 <div class="form-group row">
-                    <div class="col-md-12"><p>Name:</p>
-                    <input id="name" name="name" class="form-control input" type="text" value="{{old('name')}}">
+                    <div class="col-md-12">
+                        <p>Name:</p>
+                        <input id="name" name="name" class="form-control input" type="text" value="{{old('name')}}">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <div class="col-md-12"><p>Genetics:</p>
-                        <input id="genetics" name="genetics" class="form-control input" type="text" placeholder="genetics" value="{{$strain->genetics}}">
+                    <div class="col-md-12">
+                        <p>Genetics:</p>
+                        <input id="genetics" name="genetics" class="form-control input" type="text" placeholder="genetics"
+                            value="{{$strain->genetics}}">
                     </div>
                 </div>
                 <div class="form-group row">
                     <div class="col-md-12">
                         <p>Description:</p>
-                    <textarea name="description" class="form-control" >{{old('description')}}</textarea>
+                        <textarea name="description" class="form-control">{{old('description')}}</textarea>
                     </div>
                 </div>
                 <div class="form-group row">
@@ -73,11 +77,13 @@
                 <div class="form-group row">
                     <div class="col-md-6">
                         <label for="min_flowering_time">Min Weeks</label>
-                        <input name="min_flowering_time" type="number" min="6" max="15" default="9" id="min_flowering_time" value="{{old('min_flowering_time')}}" />
+                        <input name="min_flowering_time" type="number" min="6" max="15" default="9" id="min_flowering_time"
+                            value="{{old('min_flowering_time')}}" />
                     </div>
                     <div class="col-md-6">
                         <label for="max_flowering_time">Max Weeks</label>
-                        <input type="number" min="6" max="15" default="9" name="max_flowering_time" id="max_flowering_time" value="{{old('max_flowering_time')}}" />
+                        <input type="number" min="6" max="15" default="9" name="max_flowering_time" id="max_flowering_time"
+                            value="{{old('max_flowering_time')}}" />
                     </div>
                 </div>
                 <div class="form-group row">
@@ -96,20 +102,21 @@
 
 @section('page_scripts')
 <script>
-function previewFile() {
-  var preview = document.querySelector('#preview');
-  var file    = document.querySelector('input[type=file]').files[0];
-  var reader  = new FileReader();
+    function previewFile() {
+        var preview = document.querySelector('#preview');
+        var file = document.querySelector('input[type=file]').files[0];
+        var reader = new FileReader();
 
-  reader.addEventListener("load", function () {
-    preview.src = reader.result
-    preview.style =  'width:100%;'
-    preview.classList = 'info'
-  }, false);
+        reader.addEventListener("load", function () {
+            preview.src = reader.result
+            preview.style = 'width:100%;'
+            preview.classList = 'info'
+        }, false);
 
-  if (file) {
-    reader.readAsDataURL(file);
-  }
-}
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+    }
+
 </script>
 @stop
