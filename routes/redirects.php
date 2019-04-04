db5cead1aab74392c4e0b203c4b26b501beabe41
The Defenestrator <529446+defenestrator@users.noreply.github.com>
2019-04-04 13:54:13 -0600

    redirects for old strain urls

--- routes/redirects.php
+++ routes/redirects.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 / REDIRECT ROUTES
 */
 
-Route::redirect('/strains/1', '/strains/');
-Route::redirect('/strains/2', '/strains/');
-Route::redirect('/strains/3', '/strains/');
-Route::redirect('/strains/4', '/strains/');
+Route::redirect('/strains/1', '/strains/da43ec16-5698-11e9-8f95-eea4a075fb2e');
+Route::redirect('/strains/2', '/strains/da441bf0-5698-11e9-ac2c-eea4a075fb2e');
+Route::redirect('/strains/3', '/strains/da445f66-5698-11e9-8ebf-eea4a075fb2e');
+Route::redirect('/strains/4', '/strains/da44843c-5698-11e9-89c2-eea4a075fb2e');
 
