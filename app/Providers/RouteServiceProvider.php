3cbb39eb0edc10914eee25e8221d5eeddfe3b3a6
The Defenestrator <529446+defenestrator@users.noreply.github.com>
2019-04-04 14:31:04 -0600

    update RouteServiceProvider to include redirects.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -36,7 +36,7 @@ class RouteServiceProvider extends ServiceProvider
     public function map()
     {
         $this->mapApiRoutes();
-
+        $this->mapRedirectRoutes();
         $this->mapWebRoutes();
 
         //
@@ -69,5 +69,12 @@ class RouteServiceProvider extends ServiceProvider
              ->middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
+    }
+
+    protected function mapRedirectRoutes()
+    {
+        Route::middleware('web')
+             ->namespace($this->namespace)
+             ->group(base_path('routes/redirects.php'));
     }
 }
