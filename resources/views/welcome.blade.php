3eb0908d1cd33328a5fea08a062cbbf71b1d89ed
The Defenestrator <529446+defenestrator@users.noreply.github.com>
2019-04-15 03:21:09 -0600

    adsf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,11 +8,12 @@
         <h2>Grand Opening</h2>
     </div>
     <div class="row justify-content-center">
-        <h3>Monday April 14th, 2019!</h3>
+        <h3>Monday April 14th, 2019!? Yeesh</h3>
     </div>
     <div class="row justify-content-center">
         <h4>...turns out I need sleep.
-            <br> 3am update: I ran into some security concerns, cleaning up some potential problems before they happen. Stay tuned.</h4>
+            <br> 3am update: I ran into some security concerns, <br>
+            cleaning up some potential problems before they happen.<br> Stay tuned.</h4>
     </div>
     <strains :products="{{ $strains }}" ></strains>
     <hr />
