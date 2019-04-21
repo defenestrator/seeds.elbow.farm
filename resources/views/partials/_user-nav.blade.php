069308f5a1ec509e965f50ecaa59b3b548133df7
The Defenestrator <529446+defenestrator@users.noreply.github.com>
2019-04-20 20:25:04 -0600

    goddamn it

--- resources/views/partials/_user-nav.blade.php
+++ resources/views/partials/_user-nav.blade.php
@@ -11,7 +11,7 @@
         {{-- <a class="dropdown-item nav-link" href="{{ route('user.invoices.index') }}">Invoices</a> --}}
         <a class="dropdown-item nav-link" href="{{ route('user.shipping_addresses.index') }}">Addresses</a>
         <hr />
-        @if(Auth::user()->can('role:admin'))
+        @if(in_array(Auth::user()->email , ['jeremyblc@gmail.com', 'mike@elbowfarm.com', 'heisen@heisenbeans.com']))
         <a class="dropdown-item nav-link" href="/admin/invoices"><i class="fa fa-edit"></i> All user Invoices</a>
         <hr>
             <a class="dropdown-item nav-link" href="/admin/strains"><i class="fa fa-edit"></i> Edit Strains</a>
