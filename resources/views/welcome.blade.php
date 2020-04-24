01bc671a94ff6e4a0bc0aba4c2baf12fae18afb6
The Defenestrator <529446+defenestrator@users.noreply.github.com>
2020-04-24 15:35:14 -0600

    go

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,26 +8,12 @@
         <h1 style="font-style:italic;">Shoreline Genetics</h1>
     </div>
     <div class="row justify-content-center">
-        <h2>Artisanal Cannabis</h2>
+        <h2>Keepers in Every Pack.</h2>
     </div>
     <div class="row justify-content-center">
-        <h3>Keepers in Every Pack.</h3>
+        <h3></h3>
     </div>
     <div class="row justify-content-center">
-        <p>
-            Please report bugs
-            <a href="{{route('contact.create', ['contact_subject' =>'technical support'])}}">
-                here,
-            </a>
-            under "Technical Support."
-        </p>
     </div>
-    @auth
-    <flowers :products="{{ $flowers }}" :initial_user="'{{ Auth::user()->uuid }}'"></flowers>
-    @endauth
-
-    @guest
-    <flowers :products="{{ $flowers }}" :initial_user="'0'"></flowers>
-    @endguest
 </div>
 @endsection
